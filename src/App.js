3d97de0885d1b7e24e2983e29f469ae7541f2753
ai-lien <a.trancong@gmail.com>
2023-05-19 14:48:47 -0700

    set up board

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 // In React, a component is a piece of reusable code that represents a part of a user interface.
 // JSX element with prop className="square"
 
-export default function Square() {
+export default function Board() {
   return (
     <>
-      <button className="square">X</button>
-      <button className="square">X</button>
-      <button className="square">X</button>
-      <button className="square">X</button>
-      <button className="square">X</button>
-      <button className="square">X</button>
-      <button className="square">X</button>
-      <button className="square">X</button>
-      <button className="square">X</button>
+      <div className="board-row">
+        <button className="square">X</button>
+        <button className="square">X</button>
+        <button className="square">X</button>
+      </div>
+      <div className="board-row">
+        <button className="square">X</button>
+        <button className="square">X</button>
+        <button className="square">X</button>
+      </div>
+      <div className="board-row">
+        <button className="square">X</button>
+        <button className="square">X</button>
+        <button className="square">X</button>
+      </div>
     </>
   );
 }
