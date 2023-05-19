083997e68d7c53ca2bdd11d9631bb2612c0e4f84
ai-lien <a.trancong@gmail.com>
2023-05-19 14:54:34 -0700

    add square react component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 // In React, a component is a piece of reusable code that represents a part of a user interface.
 // JSX element with prop className="square"
 
+function Square({ value }) {
+  return <button className="square">{value}</button>;  // escape into js
+}
+
 export default function Board() {
   return (
     <>
       <div className="board-row">
-        <button className="square">X</button>
-        <button className="square">X</button>
-        <button className="square">X</button>
+        <Square value="1" />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <button className="square">X</button>
-        <button className="square">X</button>
-        <button className="square">X</button>
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <button className="square">X</button>
-        <button className="square">X</button>
-        <button className="square">X</button>
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
   );
